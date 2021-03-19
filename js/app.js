d3b0673754b45bfdd3b038331ac5ba2825c49df8
Rishika-05 <rishikaraj7263@gmail.com>
2021-03-19 23:17:43 +0530

    f

--- js/app.js
+++ js/app.js
@@ -22,6 +22,63 @@ function handleInstructionsModal() {
 // `handleInstructionsModal` function.
 $(document).ready(function(){
 	handleInstructionsModal();
+    var y = Math.floor(Math.random() * 10 + 1); 
+      
+    // counting the number of guesses 
+    // made for correct Guess 
+    var guess = 0; 
+      
+   document.getElementById("js-guess-submit").onclick = function(){ 
+      
+   // number guessed by user      
+   var x = parseInt(document.getElementById("js-user-guess").value); 
+  
+   if(x == y) 
+   {
+       guess++;
+       $("span").text(guess);
+       $("h2").text("YOU GUESSED IT!");
+   } 
+   else if(x > y) /* if guessed number is greater 
+                   than actual number*/ 
+   {     
+       guess++; 
+       $("span").text(guess);
+       $("h2").text("OOPS SORRY!! TRY A SMALLER NUMBER");
+   } 
+   else
+   { 
+       guess++;
+       $("span").text(guess); 
+       $("h2").text("OOPS SORRY!! TRY A GREATER NUMBER");
+   } 
+} 
+
+	/*newGame();
+	document.getElementById("js-guess-submit").onclick = function(){
+	var guess= document.getElementById("js-user-guess").value;
+    var feedback=getf(guess);
+    $(".count").text(count);
+    count++;
+};*/
+    
 });
 
 
+/*function newGame()
+{
+	var secretNumber = Math.floor(Math.random() * 100 + 1); 
+	var count=1;
+	
+}
+
+
+function getf(g)
+{
+   if(g> secretNumber)
+   	$("#feedback").text("Try a lower number.");
+   else if(g<secretNumber)
+   	$("#feedback").text("Try a greater number.");
+   else if(g==secretNumber)
+   	$("#feedback").text("You guessed it!");
+}*/
